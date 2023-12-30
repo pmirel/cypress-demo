8c2fbd1644ac30b44a56d043feaecd2e7a74595b
Mirel P <mirel.palii@gmail.com>
2023-12-30 22:52:04 +0200

    cypress config

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,15 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
+  defaultCommandTimeout: 10000,
+  video: true,
+  screenshotOnRunFailure: true,
+  chromeWebSecurity: false,
+  retries: 3,
+  viewportWidth: 1920,
+  viewportHeight: 1080,
   e2e: {
+    baseUrl: 'https://demoqa.com',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
