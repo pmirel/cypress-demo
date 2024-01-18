fad188b8bd4d1a882c17a80b0cd23dbf60ddb275
Mirel P <mirel.palii@gmail.com>
2024-01-18 09:41:50 +0200

    home page pom

---
+++ cypress/support/utilities/routes.js
@@ -0,0 +1,9 @@
+const routes = {
+  baseUrl: 'https://ecommerce-playground.lambdatest.io',
+  homePage: '/index.php?route=common/home',
+  specialPage: '/index.php?route=product/special',
+  blogPage: '/index.php?route=extension/maza/blog/home',
+  loginPage: '/index.php?route=account/login',
+};
+
+export default routes;
