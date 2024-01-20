1ee859852a6da58f0976a3527dd0fefacb5edf46
Mirel P <mirel.palii@gmail.com>
2024-01-20 02:40:16 +0200

    home page tests

--- cypress/e2e/homePage.cy.js
+++ cypress/e2e/homePage.cy.js
@@ -1,9 +1,45 @@
+import routes from '../support/utilities/routes';
+import { HomePage } from '../pageObjects/homePage';
+
+const homePage = new HomePage();
 describe('visit home page', () => {
   beforeEach(() => {
     cy.visit('/');
   });
 
   it('home page url is correct', () => {
-    cy.url().should('contain', 'ecommerce-playground');
+    cy.url().should('equal', 'https://ecommerce-playground.lambdatest.io/');
+  });
+
+  it('page title', () => {
+    cy.title().should('equal', 'Your Store');
+  });
+
+  it('main elements on the page are visible', () => {
+    cy.get(homePage.mainNavigation).should('be.visible');
+    cy.get(homePage.carousel).should('be.visible');
+    cy.get(homePage.topProducts).scrollIntoView().should('be.visible');
+  });
+
+  it('search from home page', () => {
+    cy.get(homePage.searchBar).find(homePage.searchInput).type('iPhone');
+
+    cy.get(homePage.searchBar).find(homePage.submitButton).click();
+    cy.url().should('contain', 'search=iPhone');
+  });
+
+  it('navigate to category mp3 players by side menu', () => {
+    cy.wait(700);
+    cy.get(homePage.sideMenuIcon).should('be.visible').click({ force: true });
+    cy.get(homePage.sideMenu).should('be.visible');
+    cy.contains('MP3 Players').click();
+
+    cy.title().should('equal', 'MP3 Players');
+  });
+
+  it('navigate to smartwatch from mega menu', () => {
+    cy.contains(homePage.classTitle, 'Mega Menu').trigger('mouseover');
+    cy.contains(homePage.smartWatchMenuElement, 'Smart Watch').click();
+    cy.title().should('equal', 'Phones & PDAs');
   });
 });
