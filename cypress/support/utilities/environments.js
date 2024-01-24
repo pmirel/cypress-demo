038c116412d1fbe762b0c5f5032400973d025b57
Mirel P <mirel.palii@gmail.com>
2024-01-24 14:04:45 +0200

    move function

--- cypress/support/utilities/environments.js
+++ cypress/support/utilities/environments.js
@@ -1,16 +1,17 @@
 // getProjectId.js
-import { getProjectId } from 'cypress';
-require('dotenv').config();
-
-export default getProjectId = () => {
+const getProjectId = () => {
   if (process.env.CYPRESS_PROJECT_ID) {
     // Running locally, use the local environment variable
     return process.env.CYPRESS_PROJECT_ID;
-  } else if (Cypress.env('CI')) {
+  } else if (Cypress.env('CI') && Cypress.env('CYPRESS_PROJECT_ID')) {
     // Running in CI, use the secrets from GitHub Actions
     return Cypress.env('CYPRESS_PROJECT_ID');
   } else {
     // Default project ID if none of the above conditions are met
     return 'default_project_id';
   }
+};
+
+module.exports = {
+  getProjectId,
 };
