781f59782c5f94e73c6a81d0026759a64c962b26
Mirel P <mirel.palii@gmail.com>
2024-01-24 14:13:07 +0200

    modify function

--- cypress/support/utilities/environments.js
+++ cypress/support/utilities/environments.js
@@ -3,9 +3,9 @@ const getProjectId = () => {
   if (process.env.CYPRESS_PROJECT_ID) {
     // Running locally, use the local environment variable
     return process.env.CYPRESS_PROJECT_ID;
-  } else if (Cypress.env('CI') && Cypress.env('CYPRESS_PROJECT_ID')) {
+  } else if (process.env.CI && process.env.CYPRESS_PROJECT_ID) {
     // Running in CI, use the secrets from GitHub Actions
-    return Cypress.env('CYPRESS_PROJECT_ID');
+    return process.env.CYPRESS_PROJECT_ID;
   } else {
     // Default project ID if none of the above conditions are met
     return 'default_project_id';
